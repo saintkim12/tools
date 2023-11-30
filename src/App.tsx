ffd9a324b408b069ef81ecf2f986779b3f0c3b9d
saintkim12 <22ini@naver.com>
2023-11-30 19:33:22 +0900

    supabase 연결 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,32 @@ import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
 
+import { createClient } from '@supabase/supabase-js'
+
+let supabase: any = null
+const getSupabase = () => {
+  if (!supabase) {
+    supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_KEY)
+  }
+  return supabase
+}
 export default function App() {
   useEffect(() => {
     const removeCounter = setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
     return removeCounter
   }, [])
+  const createAuthUser = async () => {
+    const supabase = getSupabase();
+    const { data, error } = await supabase.auth.signUp({
+      email: 'example@email.com',
+      password: 'example-password',
+    })
+    console.log('data', data)
+    console.log('error', error)
+  }
   return (
     <div>
+      <button onClick={createAuthUser}>Create user</button>
       <a href="https://vitejs.dev" target="_blank">
         <img src={viteLogo} className="logo" alt="Vite logo" />
       </a>
@@ -20,9 +39,7 @@ export default function App() {
       <div className="card">
         <button id="counter" type="button"></button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and TypeScript logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and TypeScript logos to learn more</p>
     </div>
   )
 }
