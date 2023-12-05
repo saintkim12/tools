bc362ff07b9a632dbb7334b1970e42a3f8c83f7f
saintkim12 <saintkim1232@gmail.com>
2023-12-05 13:15:24 +0900

    test data input

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,4 @@
-import { useEffect } from 'react'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
-
+import { useCallback, useState } from 'react'
 import { createClient } from '@supabase/supabase-js'
 
 let supabase: any = null
@@ -13,33 +9,37 @@ const getSupabase = () => {
   return supabase
 }
 export default function App() {
-  useEffect(() => {
-    const removeCounter = setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
-    return removeCounter
-  }, [])
-  const createAuthUser = async () => {
-    const supabase = getSupabase();
-    const { data, error } = await supabase.auth.signUp({
-      email: 'example@email.com',
-      password: 'example-password',
-    })
-    console.log('data', data)
-    console.log('error', error)
-  }
+  // useEffect(() => {
+  //   const removeCounter = setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+  //   return removeCounter
+  // }, [])
+  // const createAuthUser = async () => {
+  //   const supabase = getSupabase();
+  //   const { data, error } = await supabase.auth.signUp({
+  //     email: 'example@email.com',
+  //     password: 'example-password',
+  //   })
+  //   console.log('data', data)
+  //   console.log('error', error)
+  // }
+  const [inputText, setInputText] = useState('')
+  const [logs, setTextLogs] = useState<{ text: string; timestamp: number }[]>([])
+  const addData = useCallback(() => {
+    setTextLogs([...logs, { text: inputText, timestamp: Date.now() }])
+    setInputText('')
+  }, [logs, inputText])
   return (
     <div>
-      <button onClick={createAuthUser}>Create user</button>
-      <a href="https://vitejs.dev" target="_blank">
-        <img src={viteLogo} className="logo" alt="Vite logo" />
-      </a>
-      <a href="https://www.typescriptlang.org/" target="_blank">
-        <img src={typescriptLogo} className="logo vanilla" alt="TypeScript logo" />
-      </a>
-      <h1>Vite + TypeScript</h1>
-      <div className="card">
-        <button id="counter" type="button"></button>
+      <textarea value={inputText} onInput={(e) => setInputText(e.currentTarget.value)}></textarea>
+      <button onClick={addData}>addData</button>
+
+      <div>
+        {logs.map(({ text, timestamp }, idx) => (
+          <p key={idx}>
+            {text} :: {new Date(timestamp).toISOString()}
+          </p>
+        ))}
       </div>
-      <p className="read-the-docs">Click on the Vite and TypeScript logos to learn more</p>
     </div>
   )
 }
