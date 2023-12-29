7cc720103b03d5469fa0bafa3e02947b3339b7da
saintkim12 <saintkim1232@gmail.com>
2023-12-29 16:17:12 +0900

    작업사항저장02

---
+++ test.js
@@ -0,0 +1,17 @@
+import { S3Client, ListBucketsCommand } from '@aws-sdk/client-s3'
+
+const client = new S3Client({ region: 'ap-northeast-2', credentials: { accessKeyId: '', secretAccessKey: '' } })
+const command = new ListBucketsCommand({
+  // Bucket: 'saintkim12.resource',
+  // Key: `uploader/${file.name}`,
+  // Body: file,
+})
+
+client
+  .send(command)
+  .then((data) => {
+    console.log('data', data)
+  })
+  .catch((e) => {
+    console.error(e)
+  })
